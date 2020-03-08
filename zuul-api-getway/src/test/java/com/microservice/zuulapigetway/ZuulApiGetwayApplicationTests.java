f1d95bebc8322493f053863669e3c6516fcae672
unknown <lokabanerjee@gmail.com>
2020-03-08 22:30:11 +0530

    first commit

---
+++ zuul-api-getway/src/test/java/com/microservice/zuulapigetway/ZuulApiGetwayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.zuulapigetway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulApiGetwayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
